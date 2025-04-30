2a487157e738df91f224bcc51f388a58fd09ab5b
Daviidvl <davidovalee@icloud.com>
2025-04-30 20:37:31 -0300

    novo

--- js/index.js
+++ js/index.js
@@ -1,20 +1,3 @@
-function mostrarLogin() {
-  event.preventDefault();
-  document.getElementById('formLogin').classList.toggle('ativo');
-  document.getElementById('formCadastro').classList.remove('ativo');
-  document.getElementById('fundoEscuro').classList.toggle('ativo');
-}
 
-function mostrarCadastro() {
-  event.preventDefault();
-  document.getElementById('formCadastro').classList.toggle('ativo');
-  document.getElementById('formLogin').classList.remove('ativo');
-  document.getElementById('fundoEscuro').classList.toggle('ativo');
-}
 
-// Para fechar modal ao clicar no fundo escuro
-function fecharModal() {
-  document.getElementById('formLogin').classList.remove('ativo');
-  document.getElementById('formCadastro').classList.remove('ativo');
-  document.getElementById('fundoEscuro').classList.remove('ativo');
-}
+
